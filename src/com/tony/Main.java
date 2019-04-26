88428c10d062a5825b8e2bbd9f122bcb8bc43f35
Tony Ni <tonyni02@gmail.com>
2019-04-26 15:37:20 -0400

    updates from Mary branch

--- src/com/tony/Main.java
+++ src/com/tony/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 	// write your code here
         String testString;
-        testString = "aaa";
+        testString = "Mary";
 
         new Main().test1("Hello");
         System.out.println("Hello World!");
